30e6f609fd728d1f9f373ac52afc60170f94d768
Travis Chase <travis@travischase.me>
2025-03-04 19:44:15 -0600

    Visual updates

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,6 @@
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets/images/favicon.ico')
+  eleventyConfig.addPassthroughCopy('assets/images/logo.png')
+  eleventyConfig.addPassthroughCopy('assets/images/star-wars-unlimited-logo.webp')
   eleventyConfig.addPassthroughCopy('assets/css/site.css')
 }
