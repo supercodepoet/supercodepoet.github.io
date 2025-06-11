99e31170fc44ba0c7ac1867f050adb1dcc5de2fd
Travis Chase <travis@travischase.me>
2025-06-11 08:57:30 -0500

    Adding cache buster plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,8 @@
+import cacheBuster from '@mightyplow/eleventy-plugin-cache-buster'
+
 export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(cacheBuster({}))
+
   eleventyConfig.addPassthroughCopy('assets/images/favicon.ico')
   eleventyConfig.addPassthroughCopy('assets/images/logo.png')
   eleventyConfig.addPassthroughCopy('assets/images/star-wars-unlimited-logo.webp')
