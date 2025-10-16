695d6ff5ec1d51a5dc557b05a9a7b7c0178aa041
Travis Chase <travis@travischase.me>
2025-10-16 09:48:57 -0500

    Updates

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,13 @@
+import fs from 'node:fs'
 import cacheBuster from '@mightyplow/eleventy-plugin-cache-buster'
+import { eleventyImageTransformPlugin } from '@11ty/eleventy-img'
 
 export default function (eleventyConfig) {
+  eleventyConfig.on('eleventy.before', async ({ directories, runMode, outputMode }) => {
+    fs.cpSync('assets', '_site/assets', { recursive: true, force: true })
+  })
+
+  eleventyConfig.addWatchTarget('assets')
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin)
   eleventyConfig.addPlugin(cacheBuster({}))
 }
